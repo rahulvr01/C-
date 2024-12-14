ffeaa0792850330dd3ab57c742c48a97b7a4f552
rahulvr01 <verrahul19@gmail.com>
2024-12-14 17:07:48 +0530

    star pattern insert

--- star objects c++.cpp
+++ star objects c++.cpp
@@ -13,3 +13,19 @@ int main() {
         cout<<endl;
     }
 }
+
+#include <iostream>
+using namespace std;
+int main() {
+    for(int r=1;r<=5;++r){
+        for(int c=1;c<=5;++c){
+            if(c<=6-r){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
