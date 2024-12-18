11bccfd0f37f96b915a4f034e806242cc7e6b8a0
rahulvr01 <verrahul19@gmail.com>
2024-12-18 21:31:28 +0530

    array starting

---
+++ array.cpp
@@ -0,0 +1,11 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[]={1,2,3,4,5};
+    for(int i=0;i<5;++i){
+        if(arr[i]!=3){
+            cout<<arr[i]<<"/t";
+        }
+    }
+}
