53106c6518a9948c7dfac73e216beb59e63c810b
rahulvr01 <verrahul19@gmail.com>
2024-12-20 21:14:11 +0530

    array

---
+++ array1.cpp
@@ -0,0 +1,13 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[]={1,2,3,4,5};
+    int max=0;
+    for(int i=0;i<5;++i){
+        if(arr[i+1]>arr[i]){
+            max=arr[i+1];
+        }
+    }
+    cout<<max;
+}
